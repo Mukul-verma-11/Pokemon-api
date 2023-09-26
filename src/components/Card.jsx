50a4fd3b531455ec0c5b434d70cfbe76d1df5653
mukul <mikkiverma0311@gmail.com>
2023-09-26 16:31:39 +0530

    First commit to pokemon app

---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import "./card.css";
+import axios from "axios";
+
+function Card({ pokeurl }) {
+  const [data, setdata] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(pokeurl)
+      .then((res) => setdata(res.data))
+      .catch((err) => console.log(err));
+  }, [pokeurl]);
+
+  return (
+    <div className="container">
+      <img src={data?.sprites?.front_default} alt="image not available" />
+      <p>{data?.name}</p>
+    </div>
+  );
+}
+
+export default Card;
